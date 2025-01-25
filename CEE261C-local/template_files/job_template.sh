1bb83868abfae1c81a3f5777fa454bd8958ea076
Nicholas Bachand <ngbachand@gmail.com>
2025-01-24 22:30:02 -0800

    updating template with emails and SUID job name

--- CEE261C-local/template_files/job_template.sh
+++ CEE261C-local/template_files/job_template.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=class
+#SBATCH --job-name={SUID}-WW
 #SBATCH --partition=serc
-#SBATCH -n 32
+#SBATCH -N 2
+#SBATCH -n 64
 #SBATCH --time=12:00:00
 #SBATCH --constraint="[CLASS:SH3_CBASE|CLASS:SH3_CBASE.1|CPU_GEN:SKX]"
+#SBATCH --mail-user={SUID}@stanford.edu
+#SBATCH --mail-type=ALL
+
 # #SBATCH --mem=72G
-# #SBATCH -N 2
 
 module purge
 
