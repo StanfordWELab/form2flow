237adefd3dcd5e9cc497422cafbb50c9220ef3b4
Nicholas Bachand <ngbachand@gmail.com>
2025-12-16 11:15:29 -0800

    fixing html issue where generating from wrong timestep

--- CEE261C-local/generate_post_config.py
+++ CEE261C-local/generate_post_config.py
@@ -49,15 +49,19 @@ def main():
     probes_dir = os.path.join(fldr, "probes_results")
     
     # Search for all .pcd files in the probes directory
-    pcd_files = glob.glob(os.path.join(probes_dir, "*.pcd"))
+    pxyz_files = glob.glob(os.path.join(probes_dir, "*.pxyz"))
+    # Sort the .pxyz files based on their names
+    pxyz_files.sort()
     
     # For each .pcd file, extract the plane name and add to the configuration
-    for pcd_file in pcd_files:
-        filename = os.path.basename(pcd_file)  # e.g., door1.00060000.pcd
+    for pxyz_file in pxyz_files:
+        filename = os.path.basename(pxyz_file)  # e.g., door1.00060000.pcd
         # Use the part before the first period as the key (e.g., "door1")
         name_key = filename.split('.')[0]
         # Construct the .pxyz file path assuming it exists with the same base name
-        pxyz_file = os.path.join(probes_dir, f"{name_key}.pxyz")
+        pcd_files = glob.glob(os.path.join(probes_dir, f"{name_key}.*.pcd"))
+        final_step = max([f.split('.')[-2] for f in pcd_files])
+        pcd_file = os.path.join(probes_dir, f"{name_key}.{final_step}.pcd")
         # Add an entry to the planes dictionary
         config["planes"][name_key] = {
             "pxyz": pxyz_file,
